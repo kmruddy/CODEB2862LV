5cfcca64d3673a4029b21eacbcf39c41bdb74278
Kyle Ruddy <kmruddy@gmail.com>
2023-08-12 16:24:00 -0400

    add comments

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,6 +7,7 @@ terraform {
   }
 }
 
+# Connect to a given vCenter server 
 provider "vsphere" {
   user                 = "svc_tf@prob.local"
   password             = "Terraform!23"
@@ -14,29 +15,35 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
+# Describe to Terraform an existing vSphere datacenter
 data "vsphere_datacenter" "dc" {
   name = "Prob-DC"
 }
 
+# Describe to Terraform an existing ESXi host
 data "vsphere_host" "host" {
   name          = "vesxi00.prob.local"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+# Describe to Terraform an existing datastore
 data "vsphere_datastore" "datastore" {
   name          = "nfs-terraform"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+# Describe to Terraform an existing portgroup
 data "vsphere_network" "network" {
   name          = "VM Network"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+# Return information about the discovered datacenter 
 output "dc_info" {
   value = data.vsphere_datacenter.dc
 }
 
+# Return information about the discovered portgroup
 output "vpg_info" {
   value = data.vsphere_network.network
 }
