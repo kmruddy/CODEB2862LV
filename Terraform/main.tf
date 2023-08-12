9bcb116d4e5804a4cacd74d555ca7d354d4b7f67
Kyle Ruddy <kmruddy@gmail.com>
2023-08-12 17:03:08 -0400

    baseline

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,31 +9,31 @@ terraform {
 
 # Connect to a given vCenter server 
 provider "vsphere" {
-  user                 = "svc_tf@prob.local"
-  password             = "Terraform!23"
-  vsphere_server       = "probvcsa01.prob.local"
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
   allow_unverified_ssl = true
 }
 
 # Describe to Terraform an existing vSphere datacenter
 data "vsphere_datacenter" "dc" {
-  name = "Prob-DC"
+  name = var.dc_name
 }
 
 # Describe to Terraform an existing ESXi host
 data "vsphere_host" "host" {
-  name          = "vesxi00.prob.local"
+  name          = var.vmh_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 # Describe to Terraform an existing datastore
 data "vsphere_datastore" "datastore" {
-  name          = "nfs-terraform"
+  name          = var.ds_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 # Describe to Terraform an existing portgroup
 data "vsphere_network" "network" {
-  name          = "VM Network"
+  name          = var.pg_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
---
+++ Terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "vsphere_user" {
+  default = "svc_tf@prob.local"
+  type    = string
+}
+
+variable "vsphere_password" {
+  default = "Terraform!23"
+  type    = string
+}
+
+variable "vsphere_server" {
+  default = "probvcsa01.prob.local"
+  type    = string
+}
+
+variable "dc_name" {
+  default = "Prob-DC"
+  type    = string
+}
+
+variable "vmh_name" {
+  default = "vesxi00.prob.local"
+  type    = string
+}
+
+variable "ds_name" {
+  default = "nfs-terraform"
+  type    = string
+}
+
+variable "pg_name" {
+  default = "VM Network"
+  type    = string
+}
