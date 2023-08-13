9d0414ba0150fef1bbc6530951edbcd25bdf910c
Kyle Ruddy <kmruddy@users.noreply.github.com>
2023-08-13 14:34:30 -0400

    Update demo-script.ps1
    setting creds for the PowerCLI svc account

--- PowerCLI/demo-script.ps1
+++ PowerCLI/demo-script.ps1
@@ -1,5 +1,5 @@
 # Connect to a given vCenter server 
-Connect-VIServer -Server "probvcsa01.prob.local" -User "svc_tf@prob.local" -Password "Terraform!23"
+Connect-VIServer -Server "probvcsa01.prob.local" -User "svc_pcli@prob.local" -Password "VMware1!"
 
 # Get information back about a given datacenter 
 Get-Datacenter -Name "Prob-DC"
